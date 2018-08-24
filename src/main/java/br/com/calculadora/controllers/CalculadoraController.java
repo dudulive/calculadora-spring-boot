41cb0ba6092ffcbc805d44805ebd879aa80aaac1
edu.braz <edu.braz@N000948.maxima.local>
2018-08-24 13:49:12 -0300

    telas

--- src/main/java/br/com/calculadora/controllers/CalculadoraController.java
+++ src/main/java/br/com/calculadora/controllers/CalculadoraController.java
@@ -2,7 +2,7 @@ package br.com.calculadora.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,44 +13,49 @@ public class CalculadoraController {
 
 	private final CalculadoraService service;
 
-    @Autowired
-    public CalculadoraController(CalculadoraService service) {
-        this.service = service;
-    }
+	@Autowired
+	public CalculadoraController(CalculadoraService service) {
+		this.service = service;
+	}
 
-    @GetMapping("/soma/{primeiroValor}/{segundoValor}")
-    @ResponseBody
-    public String soma(@PathVariable  int primeiroValor, @PathVariable  int segundoValor) {
-        return service.soma(primeiroValor, segundoValor);
-    }
-    
-    @GetMapping("/subtracao/{primeiroValor}/{segundoValor}")
-    @ResponseBody
-    public String subtracao(@PathVariable  int primeiroValor, @PathVariable  int segundoValor) {
-        return service.subtracao(primeiroValor, segundoValor);
-    }
-    
-    @GetMapping("/mutiplicacao/{primeiroValor}/{segundoValor}")
-    @ResponseBody
-    public String mutiplicacao(@PathVariable  int primeiroValor, @PathVariable  int segundoValor) {
-        return service.mutiplicacao(primeiroValor, segundoValor);
-    }
-    
-    @GetMapping("/divisao/{primeiroValor}/{segundoValor}")
-    @ResponseBody
-    public String divisao(@PathVariable  int primeiroValor, @PathVariable  int segundoValor) {
-        return service.divisao(primeiroValor, segundoValor);
-    }
-    
-    @GetMapping("/potencia/{primeiroValor}/{segundoValor}")
-    @ResponseBody
-    public String potencia(@PathVariable  Double primeiroValor, @PathVariable Double segundoValor) {
-        return service.potencia(primeiroValor, segundoValor);
-    }
-    
-    @GetMapping("/raizquadrada/{valor}")
-    @ResponseBody
-    public String raizQuadra(@PathVariable  Double valor) {
-        return service.raizQuadra(valor);
-    }
+	@GetMapping("/soma")
+	@ResponseBody
+	public String soma(@RequestParam("primeiroValor") String primeiroValor,
+			@RequestParam("segundoValor") String segundoValor) {
+		return service.soma(Integer.valueOf(primeiroValor), Integer.valueOf(segundoValor));
+	}
+
+	@GetMapping("/subtracao")
+	@ResponseBody
+	public String subtracao(@RequestParam("primeiroValor") String primeiroValor,
+			@RequestParam("segundoValor") String segundoValor) {
+		return service.subtracao(Integer.valueOf(primeiroValor), Integer.valueOf(segundoValor));
+	}
+
+	@GetMapping("/mutiplicacao")
+	@ResponseBody
+	public String mutiplicacao(@RequestParam("primeiroValor") String primeiroValor,
+			@RequestParam("segundoValor") String segundoValor) {
+		return service.mutiplicacao(Integer.valueOf(primeiroValor), Integer.valueOf(segundoValor));
+	}
+
+	@GetMapping("/divisao")
+	@ResponseBody
+	public String divisao(@RequestParam("primeiroValor") String primeiroValor,
+			@RequestParam("segundoValor") String segundoValor) {
+		return service.divisao(Integer.valueOf(primeiroValor), Integer.valueOf(segundoValor));
+	}
+
+	@GetMapping("/potencia")
+	@ResponseBody
+	public String potencia(@RequestParam("primeiroValor") String primeiroValor,
+			@RequestParam("segundoValor") String segundoValor) {
+		return service.potencia(Double.valueOf(primeiroValor), Double.valueOf(segundoValor));
+	}
+
+	@GetMapping("/raizquadrada")
+	@ResponseBody
+	public String raizQuadra(@RequestParam("valor") String valor) {
+		return service.raizQuadra(Double.valueOf(valor));
+	}
 }
